4c0db985c3092aca24486961dfe0a2a3696cd10e
Jonathan Nieder <jrnieder@gmail.com>
2010-01-18 02:17:39 -0600

    debian/rules: allow simple globs in po/.gitignore
    Add a perl script to interpret simple globs and comments in
    po/.gitignore.  It is intentionally very restrictive: filenames are
    only allowed to contain alphanumeric characters, dashes, at-signs, and
    periods.
    
    The only supported glob is a * at the beginning of a filename; this
    restriction is posed not for ease of implementation but to avoid
    pretending to support full filename globbing.  We only want globs to
    match the current directory, but .gitignore syntax allows matching
    subdirectories as well, so there has to be some awkwardness here.

--- debian/clean.sh
+++ debian/clean.sh
@@ -4,9 +4,42 @@
 
 set -e
 
+remove_files='#!/usr/bin/perl
+# Remove specified files from the current directory.
+# Each line must be a filename, a comment (starting with #),
+# a simple glob (of the form *.extension), or blank.
+# Filenames are restricted to a small character set.
+
+use strict;
+use warnings;
+
+my $fnchar = qr/[-.@[:alnum:]]/;
+
+my $empty = qr/^$/;
+my $comment = qr/^#/;
+my $simple = qr/^$fnchar+$/;
+my $glob = qr/^\*$fnchar+$/;
+while (my $line = <>) {
+	chomp $line;
+	next if $line =~ $empty;
+	next if $line =~ $comment;
+
+	if ($line =~ $simple) {
+		unlink $line;
+		next;
+	}
+	if ($line =~ $glob) {
+		unlink glob($line);
+		next;
+	}
+
+	die "cannot parse $line";
+}
+'
+
 dh_testdir
 rm -fr build-aux
 rm -f ABOUT-NLS aclocal.m4 config.h.in configure
-(cd po && xargs -d "\n" rm -f) < debian/generated-po.list
-(cd m4 && xargs -d "\n" rm -f) < debian/generated-m4.list
+(cd po && perl -e "$remove_files") < debian/generated-po.list
+(cd m4 && perl -e "$remove_files") < debian/generated-m4.list
 find . -name Makefile.in -delete
